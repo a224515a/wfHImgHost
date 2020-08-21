274bb08fa15e39bbf533f0ccedb5cc9474b13ebd
a224515a <a224515a@gmail.com>
2020-08-21 23:20:46 +0800

    no message

--- listfile.bat
+++ listfile.bat
@@ -4,15 +4,15 @@ REM 檔案清單
 echo .
 echo .
 echo files in %cd% (%date% %time%)
-dir /b /od *.* > file_list.md
+REM dir /b /od *.* >file_list.md
 
 set prefix= %cd%\
 
 echo ================================================================
 REM for /f "eol=* tokens=*" %%i in (file_list.md) do (
-for /f  %%i in (file_list.md) do (
+for /f  %%i in (' dir /b /od') do (
 echo %prefix%%%i
-echo %prefix%%%i>>ok.md
+echo %prefix%%%i>>file_list.md
 )
 
 REM type file_list.md
